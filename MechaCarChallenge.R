bf9d20314cec5ebb117c865c85ecba50c3a4867b
John Ramonetti <john.ramonetti@gmail.com>
2021-05-28 13:00:25 -0400

    Deliverable 2 with images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_data)) #generate summary statistics
 
+
+
+## Summary Statistics on Suspension Coils
+
+suspension_data <- read.csv('Suspension_Coil.csv') #import dataset
+
+total_summary <- suspension_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create summary table
+
+lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>%  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create summary table
+
+
+
